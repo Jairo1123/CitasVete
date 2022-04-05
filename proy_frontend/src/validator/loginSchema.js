19053f15a59b8df9b82a459063a665d61dbae595
Jairo1123 <51216952+Jairo1123@users.noreply.github.com>
2022-04-04 18:27:57 -0600

    Delete proy_frontend directory

--- proy_frontend/src/validator/loginSchema.js
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-
-const loginSchema = Joi.object( {
-    nombre: Joi.string().required().label('Nombre'),
-    cedula: Joi.string().required().label('Contraseña')
-});
-
-export default loginSchema;
